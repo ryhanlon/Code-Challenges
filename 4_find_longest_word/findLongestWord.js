da68e88f7478fd551c9abdbc1e22a4ef2823e757
ryhanlon <ryhanlon@gmail.com>
2019-02-20 23:19:58 -0800

    feat: two versions for findLongestWord

--- 4_find_longest_word/findLongestWord.js
+++ 4_find_longest_word/findLongestWord.js
@@ -7,21 +7,70 @@ Your response should be a number.  */
 // compare each string to find the longest
 // return the int of the longest string
 
+//========================
+// function findLongestWord(str) {
+//   // initialize longestWord variable
+//   var longestWord = 1;
+//   // split string by space character
+//   var splitString = str.split(' ');
+//   splitString.filter(function(word) {
+//     if(word.length > longestWord) {
+//       longestWord = word.length;
+//     }
+//   });
+//   console.log(`This is ES5--> ${longestWord}`);
+//
+//   return longestWord;
+// }
 
-function findLongestWord(str) {
-  // initialize longestWord variable
-  var longestWord = 1;
-  // split string by space character
-  var splitString = str.split(' ');
-  splitString.filter(function(word) {
-    if(word.length > longestWord) {
-      longestWord = word.length;
-    }
-  });
-  console.log(`This is ES5--> ${longestWord}`);
 
-  return longestWord;
-}
+//==========================
+
+/*----- arrow function -----*/
+// const findLongestWord2 = str => {
+//   // initialize longestWord variable
+//   let longestWord = 1;
+//   // split string by space character
+//   let splitString = str.split(' ');
+//   splitString.filter(function(word) {
+//     if(word.length > longestWord) {
+//       longestWord = word.length;
+//     }
+//   });
+//   console.log(`This is ES6--> ${longestWord}`);
+//   return longestWord;
+// };
+
+
+//============================
+// with for of loop
+// function findLongestWord(str) {
+//   let words = str.split(" ");
+//   let num = 1;
+//   for(let word of words) {
+//     let wordL = word.length;
+//     if(wordL > num) {
+//       num = (wordL);
+//     }
+//   }
+//   console.log(num);
+//   return num;
+// }
+
+
+//=================================
+// with spread operator and Math.max()
+// function findLongestWord(str) {
+//   let words = str.split(" ");
+//   let numArray = [];
+//   for(let word of words) {
+//     numArray.push(word.length);
+//     }
+//   console.log(numArray);
+//   let longestNum = Math.max(...numArray);
+//   console.log(longestNum);
+//   return longestNum;
+// }
 
 findLongestWord("The quick brown fox jumped over the lazy dog");
 // should return 6.
@@ -33,22 +82,6 @@ findLongestWord("What is the average airspeed velocity of an unladen swallow");
 // should return 8.
 findLongestWord("What if we try a super-long word such as otorhinolaryngology");
 // should return 19.
-
-
-/*----- arrow function -----*/
-const findLongestWord2 = str => {
-  // initialize longestWord variable
-  let longestWord = 1;
-  // split string by space character
-  let splitString = str.split(' ');
-  splitString.filter(function(word) {
-    if(word.length > longestWord) {
-      longestWord = word.length;
-    }
-  });
-  console.log(`This is ES6--> ${longestWord}`);
-  return longestWord;
-};
 
 findLongestWord2("The quick brown fox jumped over the lazy dog");
 // should return 6.
