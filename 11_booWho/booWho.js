5af03fd0e44000ac838bb20ba3f2300663d48af0
ryhanlon <ryhanlon@gmail.com>
2019-02-28 20:24:48 -0800

    feat: 11_booWho

---
+++ 11_booWho/booWho.js
@@ -0,0 +1,31 @@
+// boohoo code challenge
+function booWho(bool) {
+  if(bool === true || bool === false)
+  return true;
+else {
+  return false;
+}}
+
+booWho(null);
+
+
+booWho(true);
+// should return true.
+booWho(false);
+//should return true.
+booWho([1, 2, 3]);
+//should return false.
+booWho([].slice);
+//should return false.
+booWho({ "a": 1 });
+//should return false.
+booWho(1);
+//should return false.
+booWho(NaN);
+//should return false.
+booWho("a");
+//should return false.
+booWho("true");
+//should return false.
+booWho("false");
+//should return false.
